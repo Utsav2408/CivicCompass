15c9c5dbcee4e7934e3b84c38380b23d6464e5e8
Utsav2408 <utsavvoraofficial@gmail.com>
2026-04-28 20:41:14 +0530

    fix coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -8,6 +8,7 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["./src/test/setup.ts"],
+    exclude: [...configDefaults.exclude, "e2e/**"],
     coverage: {
       provider: "v8",
       reporter: ["text", "lcov", "html"],
@@ -25,6 +26,8 @@ export default defineConfig({
         "src/main.tsx",
         "vite.config.ts",
         "vitest.config.ts",
+        "e2e/**",
+        "src/scripts/**",
       ],
     },
   },
